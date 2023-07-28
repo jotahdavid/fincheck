531d9f5b795a25c235d7132d370308b34dcab541
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-07-28 16:48:17 -0300

    feat: create AuthLayout to login and register routes

--- src/view/components/Router/index.tsx
+++ src/view/components/Router/index.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { AuthGuard } from "./AuthGuard";
-import { Login } from "../../view/pages/Login";
-import { Register } from "../../view/pages/Register";
-import { Dashboard } from "../../view/pages/Dashboard";
+import { Login } from "../../pages/Login";
+import { Register } from "../../pages/Register";
+import { Dashboard } from "../../pages/Dashboard";
+import { AuthLayout } from "../../layouts/AuthLayout";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
---
+++ src/view/layouts/AuthLayout.tsx
@@ -0,0 +1,33 @@
+import { Outlet } from "react-router-dom";
+
+import { Logo } from "../components/Logo";
+import illustrationSrc from '../../assets/illustration.png';
+
+export function AuthLayout() {
+  return (
+    <div className="flex w-full h-full">
+      <div className="w-full lg:w-1/2 h-full flex flex-col justify-center items-center">
+        <Logo className="text-gray-500 h-6" />
+
+        <div className="max-w-[504px] mt-16 w-full p-2">
+          <Outlet />
+        </div>
+      </div>
+
+      <div className="hidden w-1/2 h-full lg:flex justify-center items-center p-8 relative">
+        <img
+          src={illustrationSrc}
+          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+        />
+
+        <div className="max-w-[656px] bg-white p-10 absolute inset-8 top-auto rounded-b-[32px]">
+          <Logo className="text-teal-900 h-8" />
+
+          <p className="text-gray-700 font-medium text-xl mt-6">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

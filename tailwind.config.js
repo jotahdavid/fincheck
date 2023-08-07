2c018f08f2038b55be84a9019caaab6b9c9b8836
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-08-07 19:11:23 -0300

    feat: add "DM Sans" font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -188,6 +188,9 @@ export default {
         'overlay-show': 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
         'content-show': 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
+      fontFamily: {
+        sans: ["DM Sans", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      }
     },
   },
   plugins: [],

035930b8164d2da3f522f233c03078942f8e5c4b
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-08-08 11:14:01 -0300

    fix(AccountsSliderNavigation): create local slider state

--- src/view/pages/Dashboard/components/Accounts/AccountsSliderNavigation.tsx
+++ src/view/pages/Dashboard/components/Accounts/AccountsSliderNavigation.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import { useSwiper } from 'swiper/react';
 
@@ -6,8 +7,25 @@ interface AccountsSliderNavigationProps {
   isEnd: boolean | null;
 }
 
+interface SliderState {
+  isBeginning: boolean;
+  isEnd: boolean;
+}
+
 export function AccountsSliderNavigation({ isBeginning, isEnd }: AccountsSliderNavigationProps) {
   const swiper = useSwiper();
+
+  const [sliderState, setSliderState] = useState<SliderState>({
+    isBeginning: isBeginning ?? swiper.isBeginning,
+    isEnd: isEnd ?? swiper.isEnd,
+  });
+
+  useEffect(() => {
+    setSliderState({
+      isBeginning: isBeginning ?? swiper.isBeginning,
+      isEnd: isEnd ?? swiper.isEnd,
+    });
+  }, [isBeginning, isEnd, swiper.isBeginning, swiper.isEnd]);
 
   return (
     <div>
@@ -15,7 +33,7 @@ export function AccountsSliderNavigation({ isBeginning, isEnd }: AccountsSliderN
         type="button"
         className="py-3 pl-2.5 pr-3 rounded-full mr-1 enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
         onClick={() => swiper.slidePrev()}
-        disabled={isBeginning ?? swiper.isBeginning}
+        disabled={sliderState.isBeginning}
       >
         <ChevronLeftIcon className="text-white w-6 h-6" />
       </button>
@@ -24,7 +42,7 @@ export function AccountsSliderNavigation({ isBeginning, isEnd }: AccountsSliderN
         type="button"
         className="py-3 pl-2.5 pr-3 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
         onClick={() => swiper.slideNext()}
-        disabled={isEnd ?? swiper.isEnd}
+        disabled={sliderState.isEnd}
       >
         <ChevronRightIcon className="text-white w-6 h-6" />
       </button>
